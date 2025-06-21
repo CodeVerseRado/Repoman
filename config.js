a6ebcd8b3ae7165b671a85cf692e136e3371d89a
CodeVerseRado <jaiakashs2008@gmail.com>
2025-06-21 14:59:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+
+
+require('dotenv').config();
+
+module.exports = {
+  TOKEN: process.env.TOKEN,
+  language: "en",
+  ownerID: ["1004206704994566164"],
+  mongodbUri: process.env.MONGODB_URI,
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',
+  embedColor: "#1db954",
+  activityName: "YouTube Music",
+  activityType: "LISTENING",
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  embedTimeout: 5,
+  errorLog: "",
+  nodes: [
+    {
+      name: "GlaceYT",
+      url: "lava.link:80",
+      auth: "youshallnotpass",
+      secure: false
+    }
+  ]
+};
